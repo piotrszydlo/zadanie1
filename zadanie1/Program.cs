998a1bebb2b10cc2e50e948d882780a61e425b9f
Szydło Piotr <ps8427@bre-leasing.com.pl>
2017-11-22 22:56:07 +0100

    Dodaj pliki projektów.

---
+++ zadanie1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Aby zamknąć aplikację, należy wpisać exit i nacisnąć klawisz enter");
+            
+            while (true)
+            {
+                string input = Console.ReadLine();
+                Console.ReadKey();
+                if (input == "exit")
+                break;
+            }
+            Environment.Exit(-1);
+        }
+    }
+}
